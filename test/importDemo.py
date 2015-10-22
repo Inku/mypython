8a4d0d17ab869a2df3b0ee5ca7cb5c4b81af64b2
Inku <inku@inku.me>
2015-10-22 10:24:22 +0800

    add gitignore

---
+++ test/importDemo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: Inku
+@software: PyCharm
+@file: importDemo.py
+@time: 2015/10/22 10:18
+"""
+
+from util import fibo
+
+fibo.fib(1000)
+print(fibo.fib2(100))
+print(fibo.__name__)
---
+++ test/tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: Inku
+@software: PyCharm
+@file: tests.py
+@time: 2015/10/21 16:40
+"""
